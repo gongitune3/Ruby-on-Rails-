fc153af266af5ae70ddbbbd2be0563b05ba0bb8b
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-09 14:36:32 +0900

    [Start] 3-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ab6d1f4eaa4d5332f809dce6d2c12d4ee2a71fbd764d2eb48525e4e03813449db50d62d590d7db416cfa1bdf1cabf96aa21fec3c423b212a8dc81e61c61630bd'
